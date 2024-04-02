547e4f564c96f7d69a05e2e6013a0939c982be73
Matt Matriciano <mmatriciano@ewu.edu>
2024-04-02 14:59:14 -0700

    fixed some formatting on song card

--- personal_website/src/Components/Song.js
+++ personal_website/src/Components/Song.js
@@ -1,10 +1,14 @@
-const Song = ({song, title, albumArt, description }) => {
+const Song = ({song, genre, title, albumArt, description }) => {
     return (             
         <div className='grid justify-center'>
             <div className="flex flex-col max-w-[600px] max-h-[600px] bg-slate-300 rounded-lg text-black mx-4 my-4 shadow-xl ">
                 <div className="flex">
                     <img src={albumArt} alt='album-art' className="w-32 h-32 border border-black ml-4 mt-8"/>
-                    <h1 className=" underline font-bold mx-28 mt-20">{title}</h1>
+                    <div className="grid mx-28 mt-20">
+                        <h1 className=" underline font-bold mb-8">{title}</h1>
+                        <span className="font-bold">Genre: {genre}</span>
+                    </div>
+
                 </div>
                 
                 <div className="bg-white m-4 rounded-md shadow-xl">
